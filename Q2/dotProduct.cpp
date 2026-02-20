70e29227a0cd2b801a628f2928f0d6189eaa21c2
Kayla Humphrey <humphreykayla905@gmail.com>
2026-02-20 17:03:30 -0600

    commenting a little

--- Q2/dotProduct.cpp
+++ Q2/dotProduct.cpp
@@ -10,10 +10,9 @@ using namespace std;
 
 int main(int argc, char **argv){
     int SIZE = atoi(argv[1]);
-    //two vectors of size SIZE initialized to be all ones.
+    //two vectors of size SIZE initialized to be all twos.
     vector<int> vector1(SIZE, 2);
     vector<int> vector2(SIZE, 2);
-    //cout << "Expected output: " << SIZE << endl;
 
     //mpi init
     MPI_Init(&argc, &argv);
@@ -21,13 +20,13 @@ int main(int argc, char **argv){
     int myRank, PROCESSES;
     MPI_Comm_rank(comm, &myRank);
     MPI_Comm_size(comm, &PROCESSES);
+    double start_time = MPI_Wtime();
 
     /*This part is STRAIGHT from the textbook.
     I think its really clever!*/
     int ibegin = SIZE * (myRank) / PROCESSES;
     int iend = SIZE * (myRank + 1) / PROCESSES;
     int nsize = iend - ibegin;
-    //cout << "Ibegin : " << ibegin << " Iend: " << iend << " Nsize: " << nsize << endl;
 
     //now we need to multiply each value and add it up for ourselves.
     int local_total = 0;
@@ -35,7 +34,6 @@ int main(int argc, char **argv){
         local_total = local_total + ( vector1[index] * vector2[index] );
     }
 
-    //cout << "my local total is: " << local_total << endl;
     /*now, local_total is the dot product of our chunk of the global vector, we need to do a tree reduction 
     to combine everything*/
     int deciding_value = 0;
@@ -43,6 +41,10 @@ int main(int argc, char **argv){
     int stride = 1, source, destination;
     int recieved_value;
 
+    /*I actually tried to do this myself, I noticed that when you divide rank by stride, if that answer is an even or
+    odd number decides whether the current rank is a sender or a reciever.
+    I also noticed that all elements are done after they send once, so we can make them go away.
+    Big inspiration came from the link at the top of dotP.md*/
     while(!done && stride < PROCESSES){
         deciding_value = myRank / stride;
         
@@ -61,9 +63,11 @@ int main(int argc, char **argv){
         
         stride *= 2;
     }
-    
+
+    double end_time = MPI_Wtime();
     if(myRank == 0){
         cout << "The final sum is: " << local_total << endl;
+        cout << "This took process with rank 0, " << end_time - start_time << " seconds";
     }
     
     MPI_Finalize(); 
