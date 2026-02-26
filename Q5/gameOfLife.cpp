9fdad6905912a04cddbfcba72ccdcba07b733448
Kayla Humphrey <humphreykayla905@gmail.com>
2026-02-25 18:13:49 -0600

    ideas

---
+++ Q5/gameOfLife.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char** argv){
+    //partition out world
+    //read from file
+    //scatter parts
+    //set up loop
+    //do updates
+    //gather
+    //print out world
+}
